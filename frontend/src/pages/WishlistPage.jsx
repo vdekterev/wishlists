47ac67aefb5039d5262bfc08d51c853032e672f5
vdekterev <vlad.decterev@icloud.com>
2025-07-27 18:58:53 +0400

    Wishlisht frontend first steps

--- frontend/src/pages/WishlistPage.jsx
+++ frontend/src/pages/WishlistPage.jsx
@@ -1,9 +1,23 @@
 import { Typography } from 'antd';
+import { useEffect } from 'react';
+import api from '../api/axios.js';
 
 export default function WishlistPage() {
+  const fetchWishlists = async () => {
+    try {
+      const res = await api.get('api/wishlists');
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    void fetchWishlists();
+  }, []);
   return (
-    <Typography.Title>
-      Мои вишлисты
-    </Typography.Title>
+    <>
+      <Typography.Title>Мои вишлисты</Typography.Title>
+    </>
   );
-};
+}
